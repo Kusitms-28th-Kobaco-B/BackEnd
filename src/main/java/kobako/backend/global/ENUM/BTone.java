9d4f8028e091b24b9667a4e3b17608ab5d521366
kimyechan <tioon74@naver.com>
2024-02-29 13:58:50 +0900

    style : ENUM 데이터 주석 수정

--- src/main/java/kobako/backend/global/ENUM/BTone.java
+++ src/main/java/kobako/backend/global/ENUM/BTone.java
@@ -1,10 +1,10 @@
 package kobako.backend.global.ENUM;
 
 public enum BTone {
-    DEFAULT, // 기본형
-    REVIEW, // 리뷰형
-    ACTION, // 행동촉구형
-    WARNING, // 경고형
-    EMOTIONAL, // 감정호소형
-    PROBLEM // 문제제기형
+    DEFAULT,    // 기본형
+    REVIEW,     // 리뷰형
+    ACTION,     // 행동촉구형
+    WARNING,    // 경고형
+    EMOTIONAL,  // 감정호소형
+    PROBLEM     // 문제제기형
 }
