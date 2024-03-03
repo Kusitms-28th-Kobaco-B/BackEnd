601c5aa7060825eecaa0fe058e8dbf6b1d38a6d1
Geonwoo Pack <packdev937@gmail.com>
2024-03-03 21:42:41 +0900

    #2 feat : TrendController 추가 및 네이버 데이터 랩 환경 세팅을 진행한다

--- src/main/java/kobako/backend/global/domain/RequestUri.java
+++ src/main/java/kobako/backend/global/domain/RequestUri.java
@@ -3,5 +3,6 @@ package kobako.backend.global.domain;
 public final class RequestUri {
 
     private static final String prefix = "/api/v1";
-    public static final String crawling = "/crawling";
+    public static final String crawling = prefix + "/crawling";
+    public static final String trend = prefix + "/trend";
 }
--- src/main/java/kobako/backend/naver/application/DataLabService.java
+++ src/main/java/kobako/backend/naver/application/DataLabService.java
@@ -2,10 +2,18 @@ package kobako.backend.naver.application;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class DataLabService {
 
     @Value("${naver.datalab.client-id}")
     private String clientId;
+
+    @Value("${naver.datalab.client-secret}")
+    private String clientSecret;
+
+    @Value("${naver.datalab.base-url}")
+    private String baseUrl;
+
 }
