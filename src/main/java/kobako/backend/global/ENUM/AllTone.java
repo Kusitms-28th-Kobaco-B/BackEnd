e6f2bc144cd079ab8d165ef62a0a338adc47c7ce
kimyechan <tioon74@naver.com>
2024-03-03 23:31:20 +0900

    feat : 광고카피 ENUM값 추가

---
+++ src/main/java/kobako/backend/global/ENUM/AllTone.java
@@ -0,0 +1,13 @@
+package kobako.backend.global.ENUM;
+
+//전체 톤앤매너
+public enum AllTone {
+    DEFAULT,    // 기본형
+    REVIEW,     // 리뷰형
+    ACTION,     // 행동촉구형
+    WORDPLAY,   // 언어유희형
+    QUESTION,   // 질문형
+    WARNING,    // 경고형
+    EMOTIONAL,  // 감정호소형
+    PROBLEM     // 문제제기형
+}
