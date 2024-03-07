ab527ac6d69475aaae19e74566fba09b159e8f5b
kimyechan <tioon74@naver.com>
2024-03-08 02:26:13 +0900

    feat : swagger desciption 추가

---
+++ src/main/java/kobako/backend/swaggerUi/AdvertisementCopyUi.java
@@ -0,0 +1,54 @@
+package kobako.backend.swaggerUi;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kobako.backend.advertisementCopy.dto.request.GenerateAdvertisementCopyRequest;
+import kobako.backend.advertisementCopy.dto.request.UpdateAdvertisementCopyRequest;
+import kobako.backend.advertisementCopy.dto.response.AdvertisementCopyResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "광고카피", description = "광고카피 관련 API")
+public interface AdvertisementCopyUi {
+
+    @Operation(summary = "최근 생성한 광고카피 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "광고카피가 없음")
+    })
+    @GetMapping("/copies/recent/{memberId}")
+    ResponseEntity<List<AdvertisementCopyResponse>> getMyAdvertisementCopies(
+            @Parameter(description = "회원 ID", required = true) @PathVariable Long memberId
+    );
+
+    @Operation(summary = "새로운 광고카피 생성")
+    @PostMapping("/copies")
+    void generateAdvertisementCopy(
+            @RequestBody GenerateAdvertisementCopyRequest generateAdvertisementCopyRequest
+    );
+
+    @Operation(summary = "카피갤러리에 광고카피 저장")
+    @PostMapping("/copies/{memberId}/{advertisementCopyId}")
+    ResponseEntity<AdvertisementCopyResponse> LoadAdvertisementCopy(
+            @Parameter(description = "회원 ID", required = true) @PathVariable Long memberId,
+            @Parameter(description = "광고카피 ID", required = true) @PathVariable Long advertisementCopyId
+    );
+
+    @Operation(summary = "광고카피 수정")
+    @PatchMapping("/copies/{advertisementCopyId}")
+    ResponseEntity<AdvertisementCopyResponse> updateAdvertisementCopy(
+            @Parameter(description = "광고카피 ID", required = true) @PathVariable Long advertisementCopyId,
+            @RequestBody UpdateAdvertisementCopyRequest updateAdvertisementCopyRequest
+    );
+
+    @Operation(summary = "광고카피 삭제")
+    @DeleteMapping("/copies/{advertisementCopyId}")
+    ResponseEntity deleteAdvertisementCopies(
+            @Parameter(description = "광고카피 ID", required = true) @PathVariable Long advertisementCopyId
+    );
+}
---
+++ src/main/java/kobako/backend/swaggerUi/CopyGalleryUi.java
@@ -0,0 +1,49 @@
+package kobako.backend.swaggerUi;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kobako.backend.advertisementCopy.dto.response.AdvertisementCopyResponse;
+import kobako.backend.copyGallery.dto.Response.CopyGalleryResponse;
+import kobako.backend.global.ENUM.Service;
+import kobako.backend.global.ENUM.Tone;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Tag(name = "카피 갤러리", description = "광고 카피 갤러리 API")
+public interface CopyGalleryUi {
+
+    @Operation(summary = "최근 저장한 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "광고카피가 없음")
+    })
+    @GetMapping("/recent-loaded/{memberId}")
+    ResponseEntity<List<AdvertisementCopyResponse>> getMyRecentCopyGallery(
+            @Parameter(description = "회원 ID", required = true) @PathVariable Long memberId
+    );
+
+    @Operation(summary = "카피 갤러리 검색")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
+    @GetMapping("/search")
+    ResponseEntity<Page<CopyGalleryResponse>> GetAllAdvertismentCopies(
+            @Parameter(description = "시작 날짜") @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @Parameter(description = "종료 날짜") @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @Parameter(description = "서비스") @RequestParam(required = false) Service service,
+            @Parameter(description = "톤앤매너") @RequestParam(required = false) Tone tone,
+            @Parameter(description = "키워드") @RequestParam(required = false) String keyword
+    );
+
+}
